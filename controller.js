979f52b90e14e2877e8ddeca1edb04aa240451cf
Stephen Corry <stevemcorry@gmail.com>
2016-10-26 11:54:50 -0600

    Finish project

--- controller.js
+++ controller.js
@@ -1,10 +1,8 @@
 angular.module('userProfiles').controller('MainController', function($scope, mainService) {
-
   $scope.getUsers = function() {
-  	$scope.users = mainService.getUsers();
-  }
-
-
+    mainService.getUsers().then(function(response) {
+      $scope.users = response.data.data;
+    });
+  };
   $scope.getUsers();
-
-});
+  });
